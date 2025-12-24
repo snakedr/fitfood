3fa782edc582ad55cbf59931c71c0050dd14fcd0
snakedr <100n6@pm.me>
2025-12-24 19:52:55 +0300

    Set output
    Ensure output is set to 'static' in Astro config.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,7 @@
 import { defineConfig } from 'astro/config';
-import path from 'path'; // <-- Не забудьте импортировать 'path'
 
 // https://astro.build/config
 export default defineConfig({
-  // ... другие настройки Astro
-  vite: {
-    resolve: {
-      alias: {
-        '@': path.resolve(new URL('.', import.meta.url).pathname, './src'),
-      },
-    },
-  },
+  // Убедитесь, что вывод настроен на 'static' (по умолчанию, но лучше проверить)
+  output: 'static', 
 });
