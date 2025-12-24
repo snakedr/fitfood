090bbf59a408d54d26178c6d32fa063a579528d8
snakedr <100n6@pm.me>
2025-12-24 18:23:47 +0300

    Configure path alias for Vite in astro.config.mjs
    Added path alias configuration for Vite in Astro.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,14 @@
 import { defineConfig } from 'astro/config';
+import path from 'path'; // <-- Не забудьте импортировать 'path'
 
+// https://astro.build/config
 export default defineConfig({
-  root: './',
-  outDir: './dist',
+  // ... другие настройки Astro
+  vite: {
+    resolve: {
+      alias: {
+        '@': path.resolve(new URL('.', import.meta.url).pathname, './src'),
+      },
+    },
+  },
 });
